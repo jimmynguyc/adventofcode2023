787964941b94c4451d5d016e7a6401533ece3183
Jimmy Ngu <jimmynguyc@gmail.com>
2023-12-11 11:39:26 +0800

    day9

---
+++ day9/9-2.rb
@@ -0,0 +1,26 @@
+input = <<~INPUT.split("\n")
+  0 3 6 9 12 15
+  1 3 6 10 15 21
+  10 13 16 21 30 45
+INPUT
+input = File.read("input").split("\n")
+def groups_of_2(array)
+  array.each_cons(2).map(&:join)
+end
+
+all_series = input.map do |row|
+  ori_series = series = row.split(" ").map(&:to_i)
+  breakdown = [series]
+
+  until breakdown.last.all?(&:zero?)
+    series = series.each_cons(2).map { |a, b| b - a }
+    breakdown << series
+  end
+
+  breakdown.pop # don't care about zeroes
+
+  ori_series.unshift breakdown.reverse_each.reduce(0) { |memo, series| series.first - memo }
+end
+
+answer = all_series.sum { _1.first }
+puts answer
